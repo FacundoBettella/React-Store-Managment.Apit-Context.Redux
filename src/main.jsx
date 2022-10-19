d682d6f7b99e8bb219d4b830302900b5f90e964d
facundobettella <facundobettella@outlook.com>
2022-10-19 13:07:13 -0300

    Se avanza con Todo Context

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AppContextPractice />
+    {/* <AppContextPractice /> */}
     <TodoProvider>
       <App />
     </TodoProvider>
